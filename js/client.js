033fc8d1998ac4afb81b280801557bb1d33a0071
Ashutosh Bhadauriya <abhadauriya360@gmail.com>
2021-01-23 22:49:40 +0530

    final

--- js/client.js
+++ js/client.js
@@ -16,14 +16,6 @@ const append = (message, position) => {
   }
 };
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const message = messageInput.value;
-  append(`You: ${message}`, "right");
-  socket.emit("send", message);
-  messageInput.value = "";
-});
-
 const name = prompt("Enter your name to join");
 socket.emit("new-user-joined", name);
 
@@ -37,4 +29,12 @@ socket.on("receive", (data) => {
 
 socket.on("left", (name) => {
   append(`${name} left the chat`, "right");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
 });
